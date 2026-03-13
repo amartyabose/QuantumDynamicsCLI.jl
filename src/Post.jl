6aeffffd1dfd37320ec30fe7ae03bc5bc404fcf5
Devansh Sharma <devanshsharma.bhu2019@gmail.com>
2026-03-13 13:13:28 +0530

    Modified output file header 2

--- src/Post.jl
+++ src/Post.jl
@@ -277,12 +277,12 @@ function calculate_print_statetostate(::QDSimUtilities.Calculation"dynamics", sy
                 write(rio, "# (1)t")
                 write(iio, "# (1)t")
                 for j in axes(flows,1)
-                    write(rio, "\t# ($(j+1)) P[$i<--$j](t)")
-                    write(iio, "\t# ($(j+1)) P[$i<--$j](t)")
+                    write(rio, "\t# ($(j+1)) d/dt P[$i<--$j](t)")
+                    write(iio, "\t# ($(j+1)) d/dt P[$i<--$j](t)")
                 end
                 nbins > 1 && for j in axes(flows,1)
-                    write(rio, "\t# ($(j+1+sysdim)) P[$i<--$j](t) std")
-                    write(iio, "\t# ($(j+1+sysdim)) P[$i<--$j](t) std")
+                    write(rio, "\t# ($(j+1+sysdim)) d/dt P[$i<--$j](t) std")
+                    write(iio, "\t# ($(j+1+sysdim)) d/dt P[$i<--$j](t) std")
                 end
                 write(rio, "\n")
                 write(iio, "\n")
